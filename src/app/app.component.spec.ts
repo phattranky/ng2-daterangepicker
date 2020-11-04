c02117fb38fcf51a0f68c59430b2e9abffc2da97
Phat Tran <phat@inspectorio.com>
2020-11-04 11:26:09 +0700

    fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular9 app is running!');
+    expect(compiled.querySelector('.container h1').textContent).toContain('Angular 9');
   });
 });
